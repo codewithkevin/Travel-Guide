5403f11909732710503abf71af707e950f5b3350
codewithkevin <oseikelvin709@gmail.com>
2022-06-05 03:40:17 +0000

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,21 @@ const Header = () => {
         <section className="header">
             <img src={Hero} alt="Hero" />
             <h4>CODEWITHKEVIN</h4>
+            <h6>MREN SATCK</h6>
+
+            <div className="row">
+                <div className="col-sm-6">
+                    <button className="btn btn-primary">
+                       Hello
+                    </button>
+                </div>    
+                <div className="col-sm-6">
+                    <button className="btn btn-primary">
+                       Hello
+                    </button>
+                </div>    
+            </div>
+
         </section>    
     )
 };

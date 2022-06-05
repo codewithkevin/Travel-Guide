1c724700574928282f5067ab1eb1954ed2fab5f2
codewithkevin <oseikelvin709@gmail.com>
2022-06-05 03:31:57 +0000

    UPDATE

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+
+
+import Header from './components/Header';
+
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Header />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
+import Hero from './images/Hero.png'
 
-
+import './Header.css';
 
 const Header = () => {
 
     return (
         <section className="header">
-
+            <img src={Hero} alt="Hero" />
+            <h4>CODEWITHKEVIN</h4>
         </section>    
     )
 };

3cf10f036169202a70eb2ce257c57f61276767a8
codewithkevin <oseikelvin709@gmail.com>
2022-06-07 12:33:24 +0000

    setting up Hero section in App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import './App.css';
 
 
 import Header from './components/Header';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
+
       <Header />
+      <div>
+        <Hero />
+      </div>
+      
     </div>
+
+
   );
 }
 

db5142029cbca38cd163a34badaf6f822c060f5d
codewithkevin <oseikelvin709@gmail.com>
2022-06-08 09:41:15 +0000

    setting up her-title and her text

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,6 +5,11 @@ const Hero = () => {
     return (
         <div className="hero">
             <img src={Hero1} alt="hero" />
+            <h4 className="hero-title">JAPANESE</h4>
+            <p className="hero-text">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores alias recusandae dicta impedit voluptatibus? Veniam, sed praesentium sit pariatur cumque libero assumenda cupiditate vero maxime omnis? Tempora maxime neque amet.
+            </p>
+
         </div>
     );
 };

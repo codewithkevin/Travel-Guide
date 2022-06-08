714aa60ca17966e26d54ff73a78c1de3a19c4e71
codewithkevin <oseikelvin709@gmail.com>
2022-06-08 12:02:36 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import HeroImage from './images/japan.jpg';
 
 
 
@@ -17,7 +18,13 @@ function App() {
       </div>
 
       <div className="hero">
-        <Hero />
+        <Hero 
+        image={HeroImage}
+        location = "Japan"
+        date = "12 Jan, 2021-24 Jan, 2021"
+        description = "Japan is a country in East Asia. It is the world's most populous country, with over 127,000,000 inhabitants. It is also one of the most populous countries in the world, with over 38.6 million people living in Tokyo."
+        view = "View on Google Map"
+        />
       </div>
 
 
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,22 +1,22 @@
 import React from 'react';
 // import Hero1 from './images/japan.jpg';
 
-const Hero = () => {
+const Hero = (props) => {
     return (
         <div className="hero">
             <div className="row">
 
                 <div className="col-sm-6">
                     <div className="hero-img">
-                        <img  src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt=""/>
+                        <img src={props.image} alt="hero" />
                     </div>
                 </div> 
 
                 <div className="col-sm-6">
                     <div className="hero-text">
-                        <h1>Japan <span className="view">View on Google Map</span></h1>
-                        <p className="date">12 Jan, 2021-24 Jan, 2021</p>
-                        <p> Japan is a country in East Asia. It is the world's most populous country, with over 127,000,000 inhabitants. It is also one of the most populous countries in the world, with over 38.6 million people living in Tokyo.</p>
+                        <h1>{props.location} <span className="view">{props.view}</span></h1>
+                        <p className="date">{props.date}</p>
+                        <p>{props.description}</p>
                     </div>
                 </div> 
 

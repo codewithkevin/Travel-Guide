562d5a567b01066acd3613c3bba305c48c5e0b96
codewithkevin <oseikelvin709@gmail.com>
2022-06-08 12:26:49 +0000

    putting work into data file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import HeroImage from './images/japan.jpg';
+// import HeroImage from './images/japan.jpg';
 
 
 
 
 import Header from './components/Header';
 import Hero from './components/Hero';
+import data from './components/data';
 
 function App() {
+
+  const heroData = data.map(item => {
+    return (
+      <Hero
+        key={item.id}
+        location={item.location}
+        date={item.date}
+        description={item.description}
+        view={item.view}
+        image={item.image}
+      />
+    );
+  });
+
   return (
     <div className="App">
 
@@ -18,13 +33,7 @@ function App() {
       </div>
 
       <div className="hero">
-        <Hero 
-        image={HeroImage}
-        location = "Japan"
-        date = "12 Jan, 2021-24 Jan, 2021"
-        description = "Japan is a country in East Asia. It is the world's most populous country, with over 127,000,000 inhabitants. It is also one of the most populous countries in the world, with over 38.6 million people living in Tokyo."
-        view = "View on Google Map"
-        />
+        {heroData}
       </div>
 
 
---
+++ src/components/data.js
@@ -0,0 +1,30 @@
+const data = [
+    {
+        "id": 1,
+        "location": "Japan",
+        "date": "12 Jan, 2021-24 Jan, 2021",
+        "description": "Japan is a country in East Asia. It is the world's most populous country, with over 127,000,000 inhabitants. It is also one of the most populous countries in the world, with over 38.6 million people living in Tokyo.",
+        "view": "View on Google Map",
+        "image": "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+    },
+    {
+        "id": 2,
+        "location": "Japan",
+        "date": "12 Jan, 2021-24 Jan, 2021",
+        "description": "Japan is a country in East Asia. It is the world's most populous country, with over 127,000,000 inhabitants. It is also one of the most populous countries in the world, with over 38.6 million people living in Tokyo.",
+        "view": "View on Google Map",
+        "image": "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+    },
+    {
+        "id": 3,
+        "location": "Japan",
+        "date": "12 Jan, 2021-24 Jan, 2021",
+        "description": "Japan is a country in East Asia. It is the world's most populous country, with over 127,000,000 inhabitants. It is also one of the most populous countries in the world, with over 38.6 million people living in Tokyo.",
+        "view": "View on Google Map",
+        "image": "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+    },
+
+]
+
+export default data;
+
